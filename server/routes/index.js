3036babb6e812f14135d046bb8460c3db37cee2e
Venkatesh <mvenki2014@gmail.com>
2019-05-23 17:49:33 +0530

    Email seding by smtp and File upload using Nodejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 const bodyParser = require('body-parser');
-const {Pool, Client} = require('pg');
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+var nodemailer = require('nodemailer');
+const router = express.Router();
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,  //true for 465 port, false for other ports
+    service: 'gmail',
+    auth: {
+        user: 'venkatesh.m@fininfocom.com',
+        pass: 'Venkisingle123?'
+    }
+});
+
+
+router.use(bodyParser.json());
+/*const {Pool, Client} = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
@@ -18,9 +36,7 @@ const db = new Client({
 });
 db.connect();
 
-const router = express.Router();
 
-router.use(bodyParser.json());
 
 router.get('/getuser', function (req, res) {
     pool.query('SELECT * from reg_users', (err, dbRes) => {
@@ -43,6 +59,57 @@ router.post('/postuser', function (req, res) {
             req.body.name,
             req.body.phone,
         ]
+    });
+});*/
+
+router.get('/getuser', function (req, res) {
+    res.send({type: 'get', data: ['getServiceUsers serice']});
+});
+
+router.get('/', function (req, res) {
+    res.send({type: 'get', data: ['getService', req.id]});
+});
+
+router.post('/upload', function (req, res, next) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+
+        //Path where image will be uploaded
+        fstream = fs.createWriteStream('./public/uploads/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            console.log("Upload Finished of " + filename);
+            res.redirect('/thanks.html');           //where to go next
+        });
+    });
+});
+
+
+router.post('/postMail', function (req, res) {
+    var mailOptions = {
+        from: 'venkatesh.m@fininfocom.com',
+        to: req.body.email,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!',
+        html: '<h1>Welcome</h1><p>That was easy!</p>'
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.msg = 'Mail has Error' + error;
+        } else {
+            res.msg = 'Email sent: ' + info.response;
+        }
+        res.send({
+            type: 'POST',
+            data: {
+                email: req.body.email
+            },
+            response: {
+                "message": res.msg
+            }
+        })
     });
 });
 
